6ba54a839220efb55431fe1f32ee2d81024dfb0d
Quantum Robotics <74209324+QuantumRoboticsURC@users.noreply.github.com>
2025-02-01 09:59:40 -0600

    Delete cv_ros/build directory

--- cv_ros/build/computer_ros_vision/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/quantum_main/computer_vision/cv_ros/install/computer_ros_vision'
--- cv_ros/build/custom_interfaces/custom_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/quantum_main/computer_vision/cv_ros/build/custom_interfaces/custom_interfaces__py
-# Build directory: /home/quantum_main/computer_vision/cv_ros/build/custom_interfaces/custom_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
